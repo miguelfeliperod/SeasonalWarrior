049172ecb6999cab9e5df014e08270ca21ed9a17
miguel <miguelfeliperod@gmail.com>
2021-12-10 00:21:28 -0300

    balanceamento de movimento

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public const int MAX_SPEED = 5;
+    public const float MAX_SPEED = 6f;
     public Rigidbody2D rb;
     public Collider2D characterCollider;
 
@@ -13,32 +13,20 @@ public class CharacterMovement : MonoBehaviour
     public float yAcceleration;
     private Vector2 characterSpeed = Vector2.zero;
     private Vector2 jumpSpeed = Vector2.zero;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        characterSpeed.x = xAcceleration;
-        jumpSpeed.y = yAcceleration;
-    }
 
-    // Update is called once per frame
+    void Awake() {
+         jumpSpeed = new Vector2(0, yAcceleration);
+    }
+        
     void Update()
     {
-        characterSpeed.x = xAcceleration;
-        jumpSpeed.y = yAcceleration;
-
-        if(Input.GetKey(KeyCode.RightArrow)){
-            rb.AddForce(characterSpeed);
-            if(rb.velocity.x > MAX_SPEED)
-                rb.velocity = new Vector2(MAX_SPEED, rb.velocity.y);
-        } 
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            rb.AddForce(-characterSpeed);
-            if(rb.velocity.x < -MAX_SPEED)
-                rb.velocity = new Vector2(-MAX_SPEED, rb.velocity.y);
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + xAcceleration, 0, MAX_SPEED), rb.velocity.y);
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow)){
-            print("Jummped");
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x - xAcceleration, -MAX_SPEED, 0), rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
             rb.AddForce(jumpSpeed);
-        }
     }
 }
